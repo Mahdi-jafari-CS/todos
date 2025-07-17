23b2e786d6b8c462edba13984912c380ca1fc174
Mahdi-jafari-CS <mahdijafary4433@gmail.com>
2025-07-17 23:47:05 +0500

    project created

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+ import { useState } from 'react'
+import './App.css'
+import { Header } from './components/Header'
+import { Tabs } from './components/Tabs'
+import { TodoList } from './components/TodoList'
+import { TodoInput } from './components/TodoInput'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Tabs />
+      <TodoList />
+      <TodoInput />
+      </>
+  )
+}
+
+export default App
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,7 @@
+export function Tabs() {
+    return(
+        <nav>
+            this is tha tabs
+        </nav>
+    )
+}
